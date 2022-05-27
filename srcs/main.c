3d46d15e22a20591027e2ef9aaa7c383c3cb5559
Alienxbe <martin.ykman@gmail.com>
2022-05-27 09:34:32 +0200

    Merge

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maykman <maykman@student.s19.be>           +#+  +:+       +#+        */
+/*   By: mykman <mykman@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 22:44:40 by maykman           #+#    #+#             */
-/*   Updated: 2022/05/26 23:12:23 by maykman          ###   ########.fr       */
+/*   Updated: 2022/05/26 23:37:25 by mykman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 void	draw(t_data *d)
 {
-	mlx_clear_window(d->mlx_ptr, d->mlx_win);
+	// mlx_clear_window(d->mlx_ptr, d->mlx_win);
 	for (int y = 0; y <= WIN_HEIGHT / TILE_SIZE; y++)
 		for (int x = 0; x <= WIN_WIDTH / TILE_SIZE; x++)
 			set_tile(*d, d->assets.tiles, grass, (t_point){x, y});
-	for (int i=0; i < 400; i++)
-		set_tile(*d, d->assets.tiles, i, (t_point){i % 8, i / 8});
-	for (int i = 0; i < 16; i++)
-		set_tile(*d, d->assets.tiles, grass_pkmn, ft_new_point(5 + i % 4, 5 + i / 4));
+	// for (int i=0; i < 400; i++)
+	// 	set_tile(*d, d->assets.tiles, i, (t_point){i % 8, i / 8});
+	// for (int i = 0; i < 16; i++)
+	// 	set_tile(*d, d->assets.tiles, grass_pkmn, ft_new_point(5 + i % 4, 5 + i / 4));
 	// rendering player
 	set_tile(*d, d->assets.player, d->game.player.frame / (FRAME_PER_ANIMATION) + d->game.player.rot * 4, d->game.player.pos);
 }
