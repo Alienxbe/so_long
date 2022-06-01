7748bf63a308435b06b15e1c3f1e0f50bffc9591
Alienxbe <martin.ykman@gmail.com>
2022-06-01 17:20:38 +0200

    Merging

--- srcs/builder/draw.c
+++ srcs/builder/draw.c
@@ -6,7 +6,7 @@
 /*   By: mykman <mykman@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 13:20:28 by maykman           #+#    #+#             */
-/*   Updated: 2022/05/29 00:07:25 by mykman           ###   ########.fr       */
+/*   Updated: 2022/05/29 01:01:13 by mykman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 void	draw(t_data *d)
 {
-	char	*fps_str;
-
 	for (int l = 0; l < d->map.layer_count; l++)
 		for (int y = 0; y < d->map.size.y; y++)
 			for (int x = 0; x < d->map.size.x; x++)
